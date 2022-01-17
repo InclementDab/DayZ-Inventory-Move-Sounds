709ea9e643d6636eeba85edcbafb23341274e2eb
George <jiri.huss@seznam.cz>
2022-01-17 16:19:38 +0100

    Sound Corrections - Update01
    - updated for DayZ 1.15
    - changed more than a hundred soundsets for items that were not accurate, unrealistic, or too loud or quiet
    - created several other soundets for some items
    - merged "CfgSoundShaders" and "CfgSoundSets" into one config for better clarity, as it is in Vanilla.
    - clarification of the whole MoveSounds config with BIG TEXT

--- InventoryMoveSounds/Scripts/4_World/InventoryMoveSounds/ItemBase.c
+++ InventoryMoveSounds/Scripts/4_World/InventoryMoveSounds/ItemBase.c
@@ -2,8 +2,8 @@ modded class ItemBase
 {
 	// Settings
 	static const float MAX_SOUND_RANGE = 25; // Mainly in place to stop bleed at long range
-	static const float MIN_SOUND_VOLUME = 0;
-	static const float MAX_SOUND_VOLUME = 1;
+	static const float MIN_SOUND_VOLUME = 0.45;
+	static const float MAX_SOUND_VOLUME = 0.85;
 	static const int MAX_ITEM_AREA = 12;
 	
 	override void EEItemLocationChanged(notnull InventoryLocation oldLoc, notnull InventoryLocation newLoc)
@@ -30,7 +30,6 @@ modded class ItemBase
 		ConfigGetTextArray("ItemMoveSounds", sounds);		
 	
 		float volume = ConfigGetFloat("ItemMoveSoundVolume");
-		float sound_volume = Math.Clamp(GetItemArea(this) / MAX_ITEM_AREA, MIN_SOUND_VOLUME, MAX_SOUND_VOLUME);
 		EffectSound sound = SEffectManager.CreateSound(sounds.GetRandomElement(), GetPosition());
 		if (sound) {
 			sound.SetSoundAutodestroy(true);
@@ -40,12 +39,13 @@ modded class ItemBase
 			}
 		}
 		
+		float sound_volume = Math.Clamp(GetItemArea(this) / MAX_ITEM_AREA, MIN_SOUND_VOLUME, MAX_SOUND_VOLUME);
 		EffectSound move_sound = SEffectManager.CreateSound("IMS_Item_Move_Regular", GetPosition());
 		if (move_sound) {
 			move_sound.SetSoundAutodestroy(true);
 			move_sound.SoundPlay();
 			if (move_sound.IMSGetWave()) {
-				//move_sound.IMSGetWave().SetVolume(sound_volume);
+				move_sound.IMSGetWave().SetVolume(sound_volume);
 			}
 		}
 	}
--- InventoryMoveSounds/Sounds/config.cpp
+++ InventoryMoveSounds/Sounds/config.cpp
@@ -11,14 +11,211 @@ class CfgPatches
 	};
 };
 
+class CfgSoundSets
+{
+    class IMS_SoundSet_Base
+    {
+		sound3DProcessingType = "character3DProcessingType";
+		volumeCurve = "characterAttenuationCurve";
+		spatial = 1;
+		doppler = 0;
+		loop = 0;
+        distanceFilter = "defaultDistanceFilter";
+    };
+	class IMS_PipeWrench_SoundSet: IMS_Soundset_Base
+	{
+		soundShaders[]=
+		{
+			"IMS_PipeWrench_Soundshader"
+		};
+		frequencyFactor=0.15;
+	};
+	class IMS_CombinationLock_SoundSet: IMS_Soundset_Base
+	{
+		soundShaders[]=
+		{
+			"IMS_CombinationLock_Soundshader"
+		};
+	};
+	class IMS_SmallCan_SoundSet: IMS_Soundset_Base
+	{
+		soundShaders[]=
+		{
+			"IMS_SmallCan_Soundshader"
+		};
+		frequencyRandomizer=0.5;
+		frequencyFactor=1.50;
+		volumeRandomizer=1;
+	};
+	class IMS_TinyCan_SoundSet: IMS_Soundset_Base
+	{
+		soundShaders[]=
+		{
+			"IMS_TinyCan_Soundshader"
+		};
+		frequencyRandomizer=0.5;
+		frequencyFactor=3.00;
+		volumeRandomizer=1;
+	};
+	class IMS_MagazineBase_Soundset: IMS_Soundset_Base
+	{
+		soundShaders[]=
+		{
+			"IMS_Magazine_DropMag_SoundShader",
+			"IMS_Magazine_DropAmmo_SoundShader"
+		};
+		frequencyFactor=0.75;
+	};
+	
+	class IMS_ShotgunShell_Move_Smoll: IMS_SoundSet_Base
+    {
+        soundShaders[] = {"IMS_ShotgunShell_Move_SoundShader"};
+    };
+	
+	class IMS_Bullet_Move_Smoll: IMS_SoundSet_Base
+    {
+        soundShaders[] = {"IMS_Bullet_Move_SoundShader"};
+    };
+
+    class IMS_Item_Move_Small: IMS_SoundSet_Base
+    {
+        soundShaders[] = {"IMS_Item_Move_Small_SoundShader"};
+    };
+    
+    class IMS_Item_Move_Regular: IMS_SoundSet_Base
+    {
+        soundShaders[] = {"IMS_Item_Move_Regular_SoundShader"};
+    };
+	
+	class IMS_GrenadeBop_Metal: IMS_SoundSet_Base
+    {
+        soundShaders[] = {"IMS_GrenadeBop_Metal_SoundShader"};
+    };
+	
+    class IMS_GrenadeBop_Concrete: IMS_SoundSet_Base
+    {
+        soundShaders[] = {"IMS_GrenadeBop_Concrete_SoundShader"};
+    };
+	
+    class IMS_GrenadeBop_Dirt: IMS_SoundSet_Base
+    {
+        soundShaders[] = {"IMS_Grebadenop_Dirt_SoundShader"};
+    };
+	
+    class IMS_GrenadeBop_Gravel: IMS_SoundSet_Base
+    {
+        soundShaders[] = {"IMS_Grebadenop_Gravel_SoundShader"};
+    };
+	
+    class IMS_GrenadeBop_Grass: IMS_SoundSet_Base
+    {
+        soundShaders[] = {"IMS_Grebadenop_Grass_SoundShader"};
+    };
+};
+
 class CfgSoundShaders
 {
     class IMS_SoundShader_Base
+	{
+		range=25;
+	};
+	class IMS_MagazineMove_Base
+    {
+		range = 12;
+        radius = 1.5;
+    };
+	class IMS_BulletMove_Base
     {
         volume = 3;
 		range = 7.5;
         radius = 1.5;
     };
+	class IMS_GrenadeBop_Base
+    {
+        volume = 3;
+		range = 25;
+        radius = 0.01;
+    };
+	
+	class IMS_PipeWrench_Soundshader: IMS_SoundShader_Base
+	{
+		samples[]=
+		{			
+			{"DZ\sounds\Characters\actions\items\wrench\wrench_drop", 1}
+		};
+	};
+	class IMS_CombinationLock_Soundshader: IMS_SoundShader_Base
+	{
+		samples[]=
+		{			
+			{"\DZ\sounds\Characters\actions\pickup\pickUpBackpackMetal_1",1},
+			{"\DZ\sounds\Characters\actions\pickup\pickUpBackpackMetal_2",1},
+			{"\DZ\sounds\Characters\actions\pickup\pickUpBackpackMetal_3",1},
+			{"\DZ\sounds\Characters\actions\pickup\pickUpBackpackMetal_4",1},
+			{"\DZ\sounds\Characters\actions\pickup\pickUpBackpackMetal_5",1},
+			{"\DZ\sounds\Characters\actions\pickup\pickUpBackpackMetal_6",1}
+		};
+	};
+	class IMS_SmallCan_Soundshader: IMS_SoundShader_Base
+	{
+		samples[]=
+		{			
+			{"DZ\sounds\Characters\actions\items\BakedBeansCan\BakedBeansCan_pickup_1",1},
+			{"DZ\sounds\Characters\actions\items\BakedBeansCan\BakedBeansCan_pickup_2",1},
+			{"DZ\sounds\Characters\actions\items\BakedBeansCan\BakedBeansCan_pickup_3",1},
+			{"DZ\sounds\Characters\actions\items\BakedBeansCan\BakedBeansCan_pickup_4",1},
+			{"DZ\sounds\Characters\actions\items\BakedBeansCan\BakedBeansCan_pickup_5",1}
+		};
+		volume=0.80;
+	};
+	class IMS_TinyCan_Soundshader: IMS_SoundShader_Base
+	{
+		samples[]=
+		{			
+			{"DZ\sounds\Characters\actions\items\BakedBeansCan\BakedBeansCan_pickup_1",1},
+			{"DZ\sounds\Characters\actions\items\BakedBeansCan\BakedBeansCan_pickup_2",1},
+			{"DZ\sounds\Characters\actions\items\BakedBeansCan\BakedBeansCan_pickup_3",1},
+			{"DZ\sounds\Characters\actions\items\BakedBeansCan\BakedBeansCan_pickup_4",1},
+			{"DZ\sounds\Characters\actions\items\BakedBeansCan\BakedBeansCan_pickup_5",1}
+		};
+		volume=0.70;
+	};
+	
+	class IMS_Magazine_DropMag_SoundShader: IMS_MagazineMove_Base
+	{
+		samples[]=
+		{			
+			{"DZ\sounds\Characters\movement\weapons\Base_Tail_1", 1},
+			{"DZ\sounds\Characters\movement\weapons\Base_Tail_3", 1},
+			{"DZ\sounds\Characters\movement\weapons\Base_Tail_4", 1},
+			{"DZ\sounds\Characters\movement\weapons\Base_Tail_5", 1},
+			{"DZ\sounds\Characters\movement\weapons\Base_Tail_6", 1},
+			{"DZ\sounds\Characters\movement\weapons\Base_Tail_7", 1}
+		};
+		volume = 1.50;
+	};
+	class IMS_Magazine_DropAmmo_SoundShader: IMS_MagazineMove_Base
+	{
+		samples[]=
+		{			
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_1",1},
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_2",1},
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_3",1},
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_4",1},
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_5",1},
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_6",1},
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_7",1},
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_8",1},
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_9",1},
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_10",1},
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_11",1},
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_12",1},
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_13",1},
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_14",1},
+			{"DZ\sounds\Characters\movement\weapons\MagPistol_empty_loop_15",1}
+		};
+		volume = 0.25;
+	};
 	
 	class IMS_ShotgunShell_Move_SoundShader: IMS_SoundShader_Base
     {
@@ -47,7 +244,7 @@ class CfgSoundShaders
     };
 	
 	
-    class IMS_Item_Move_Small_SoundShader: IMS_SoundShader_Base
+    class IMS_Item_Move_Small_SoundShader: IMS_BulletMove_Base
     {
         samples[] = 
         {
@@ -70,7 +267,7 @@ class CfgSoundShaders
         };
     };
 
-    class IMS_Item_Move_Regular_SoundShader: IMS_SoundShader_Base
+    class IMS_Item_Move_Regular_SoundShader: IMS_BulletMove_Base
     {
         range = 13.5;
         samples[] = 
@@ -93,37 +290,65 @@ class CfgSoundShaders
             {"DZ\sounds\Characters\movement\attachment\Backpack\military\sprintErc_8", 1}
         };
     };
-};
-
-class CfgSoundSets
-{
-    class IMS_SoundSet_Base
+	
+	class IMS_GrenadeBop_Concrete_SoundShader: IMS_GrenadeBop_Base
     {
-		sound3DProcessingType = "character3DProcessingType";
-		volumeCurve = "characterAttenuationCurve";
-		spatial = 1;
-		doppler = 0;
-		loop = 0;
-        distanceFilter = "defaultDistanceFilter";
-    };
-
-	class IMS_ShotgunShell_Move_Smoll: IMS_SoundSet_Base
-    {
-        soundShaders[] = {"IMS_ShotgunShell_Move_SoundShader"};
+        samples[] = 
+        {
+            {"DZ\sounds\Characters\movement\footstep\asphalt_ext\shared\sprinterc_7", 1},
+            {"DZ\sounds\Characters\movement\footstep\asphalt_ext\shared\sprinterc_5", 1},
+            {"DZ\sounds\Characters\movement\footstep\asphalt_ext\shared\walkerc_5", 1},
+            {"DZ\sounds\Characters\movement\footstep\asphalt_ext\shared\sprinterc_2", 1},
+            {"DZ\sounds\Characters\movement\footstep\asphalt_ext\shared\sprinterc_6", 1}
+        };
     };
 	
-	class IMS_Bullet_Move_Smoll: IMS_SoundSet_Base
+	class IMS_GrenadeBop_Metal_SoundShader: IMS_GrenadeBop_Base
     {
-        soundShaders[] = {"IMS_Bullet_Move_SoundShader"};
+        samples[] = 
+        {
+            {"DZ\sounds\Characters\movement\footstep\metal_thick_int\shared\sprinterc_3", 1},
+            {"DZ\sounds\Characters\movement\footstep\metal_thick_int\shared\sprinterc_4", 1},
+            {"DZ\sounds\Characters\movement\footstep\metal_thick_int\shared\sprinterc_6", 1},
+            {"DZ\sounds\Characters\movement\footstep\metal_thick_int\shared\sprinterc_7", 1},
+            {"DZ\sounds\Characters\movement\footstep\metal_thick_int\shared\sprinterc_5", 1}
+        };
     };
-
-    class IMS_Item_Move_Small: IMS_SoundSet_Base
+	
+	class IMS_Grebadenop_Dirt_SoundShader: IMS_GrenadeBop_Base
     {
-        soundShaders[] = {"IMS_Item_Move_Small_SoundShader"};
+        volume = 50;
+        samples[] = 
+        {
+            {"DZ\sounds\Characters\movement\footstep\dirt_int\shared\runerc_8", 1},
+            {"DZ\sounds\Characters\movement\footstep\dirt_int\shared\sprinterc_1", 1},
+			{"DZ\sounds\Characters\movement\footstep\dirt_int\shared\walkerc_3", 1},
+			{"DZ\sounds\Characters\movement\footstep\dirt_int\shared\runerc_3", 1},
+			{"DZ\sounds\Characters\movement\footstep\dirt_int\shared\sprinterc_4", 1}
+        };
     };
-    
-    class IMS_Item_Move_Regular: IMS_SoundSet_Base
+	
+	class IMS_Grebadenop_Gravel_SoundShader: IMS_GrenadeBop_Base
     {
-        soundShaders[] = {"IMS_Item_Move_Regular_SoundShader"};
+        samples[] = 
+        {
+            {"DZ\sounds\Characters\movement\footstep\gravel_small_ext\shared\runerc_1", 1},
+            {"DZ\sounds\Characters\movement\footstep\gravel_small_ext\shared\runerc_2", 1},
+            {"DZ\sounds\Characters\movement\footstep\gravel_small_ext\shared\sprinterc_3", 1},
+            {"DZ\sounds\Characters\movement\footstep\gravel_small_ext\shared\sprinterc_3", 1},
+            {"DZ\sounds\Characters\movement\footstep\gravel_small_ext\shared\sprinterc_2", 1}
+        };
+    };
+	
+	class IMS_Grebadenop_Grass_SoundShader: IMS_GrenadeBop_Base
+    {
+        samples[] = 
+        {
+            {"DZ\sounds\Characters\movement\footstep\cp_grass_tall\boots\scufferc_1", 1},
+            {"DZ\sounds\Characters\movement\footstep\cp_grass_tall\boots\scufferc_3", 1},
+			{"DZ\sounds\Characters\movement\footstep\cp_grass_tall\boots\scufferc_5", 1},
+			{"DZ\sounds\Characters\movement\footstep\cp_grass_tall\boots\scufferc_7", 1},
+			{"DZ\sounds\Characters\movement\footstep\cp_grass_tall\boots\scufferc_8", 1}
+        };
     };
 };
